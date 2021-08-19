1b844358eb67ee9be933131839bbee89350d152b
lacoalition <communitycoalition@github.com>
2021-08-19 14:15:53 -0700

    map update

--- js/index.js
+++ js/index.js
@@ -1,52 +1,72 @@
 //set up base map
-var mymap = L.map('mapid').setView([34.041581, -118.221645], 12);
+var mymap = L.map('mapid').setView([34.01712330775451, -118.26961622176387], 13);
 var bounds = [
     [34.056659, -118.220425], //southwest
     [34.008636, -118.219770]
 ];
 
-L.tileLayer.provider('Stamen.TonerLite', {
-  id: 'mapbox.streets',
-  minZoom: 1,  
-  maxBounds: bounds,
-  trackResize: false,
-  accessToken: 'pk.eyJ1IjoibWFkZWJ5YyIsImEiOiJjampwOWYyNnA3d240M3ZsZnIwODN4ZGl5In0.XFXCZd4wqKFsB7jjH0dUOQ'
-  }).addTo(mymap);
+// L.tileLayer.provider('Stamen.TonerLite', {
+//   id: 'mapbox.streets',
+//   minZoom: 1,  
+//   maxBounds: bounds,
+//   trackResize: true,
+//   accessToken: 'pk.eyJ1IjoibWFkZWJ5YyIsImEiOiJjampwOWYyNnA3d240M3ZsZnIwODN4ZGl5In0.XFXCZd4wqKFsB7jjH0dUOQ'
+//   }).addTo(mymap);
+L.tileLayer('https://api.mapbox.com/styles/v1/madebyc/cjjp9qx7zahic2rthupyi6zzw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFkZWJ5YyIsImEiOiJjampwOWYyNnA3d240M3ZsZnIwODN4ZGl5In0.XFXCZd4wqKFsB7jjH0dUOQ').addTo(mymap);
 
 var redIcon = L.icon({
-  iconUrl: 'target.png',
-  iconSize: [35, 35], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  shadowAnchor: [4, 62], // the same for the shadow
-//   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  iconUrl: 'surveillance.svg',
+  iconSize: [25, 25], // size of the icon
   });
 var k_icon = L.icon({
-  iconUrl: 'marker.svg',
+  iconUrl: 'candle3.svg',
   iconSize: [35, 35], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  shadowAnchor: [4, 62], // the same for the shadow
-  //   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var jesse_icon = L.icon({
+  iconUrl: 'JESSE.png',
+  iconSize: [35, 35], // size of the icon
 });
 
   d3.csv('laser_zones.csv', function(error, data) {
 
     if (error) throw error;
 
+    let divisionColor = "red";
     data.forEach(function(row) {
-
+      switch (row['DIVISION']) {
+        case "NEWTON":
+          divisionColor = "#2D2424";
+          break;
+        case "SOUTHWEST":
+          divisionColor = "#B85C38";
+          break;
+        case "HARBOR": 
+          divisionColor = "#002366";
+          break;
+        case "HOLLENBACK": 
+          divisionColor = "#7952B3";
+          break;
+        case "77th Street ":
+          divisionColor = "#BD1616";
+          break;
+        case "Southeast":
+          divisionColor = "#FFD523";
+          break;
+        default: 
+          divisionColor = "red";
+      }
       var bounds = JSON.parse((row['BOUNDS']))
       var polyline = L.polyline(bounds, {
-        color: 'red',
-        weight: 2,
-        opacity: .7,
+        color: divisionColor,
+        weight: 3,
+        opacity: 1,
         lineJoin: 'round',
-        fill: 'red',
+        fill: true,
       }).addTo(mymap);
-
-      polyline.bindPopup(row['DIVISION'] + " DIVISION : " + row['LASER ID']);
-      polyline.addEventListener('mouseover', function() {
-        polyline.openPopup();
-      })
+      let divisionName = row['DIVISION'] + " "+ row['LASER ID']
+      polyline.bindTooltip(divisionName, { direction: 'bottom', opacity: 1, permanent: false, className: "my-label", offset: [0, 0] });
+      polyline.openPopup();
 
     }); //end for loop
 
@@ -68,10 +88,14 @@ d3.csv('anchor_points.csv', function (error, data) {
         "<br><b>Address: </b>" + row['AP Address'] +
         "<br><b>Type: </b>" + row['Property type'] +
         "<br><b>Details: </b>" + row['Property detail/name']
-      );
+      , {autoClose: true});
       marker.addEventListener('mouseover', function() {
         marker.openPopup();
       })
+      marker.addEventListener('mouseout', function () {
+        marker.closePopup();
+      })
+
     }
   }); //end for loop
 
@@ -84,10 +108,13 @@ d3.csv('lapd_killings.csv', function (error, data) {
   data.forEach(function (row) {
     let x = row['LATITUDE '];
     let y = row['LONGITUDE'];
-    console.log(x, y);
+    let jesse = null;
+    if (row['NAME'] === "Jesse Romero") {
+      jesse = jesse_icon;
+    }
     if ( x != null && y != null) {
       let k_marker = L.marker([x, y], {
-        icon: k_icon
+        icon: jesse ? jesse_icon : k_icon,
       }).addTo(mymap);
 
       k_marker.bindPopup(
@@ -95,9 +122,8 @@ d3.csv('lapd_killings.csv', function (error, data) {
         "<br><b>Age: </b>" + row['AGE'] +
         "<br><b>Date: </b>" + row['DATE'] +
         "<br><b>Killer Officer Type: </b>" + row['OFFICER-TYPE'] +
-        "<br><b>Division: </b>" + row['DIVISION'] +
-        "<br><b>Circumstances: </b>" + row['CIRCUMSTANCES']
-      );
+        "<br><b>Division: </b>" + row['DIVISION'] 
+        );
       k_marker.addEventListener('mouseover', function () {
         k_marker.openPopup();
       })
